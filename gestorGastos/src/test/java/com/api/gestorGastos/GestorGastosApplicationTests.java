cbc833048e487ccbd688e5c5d0885d4e5557e927
SergioPicard <sergiofju7@gmail.com>
2023-10-27 17:03:08 -0300

    instalamos proyecto

---
+++ gestorGastos/src/test/java/com/api/gestorGastos/GestorGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gestorGastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
